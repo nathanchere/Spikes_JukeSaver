3fb89cf5ee9c4d6a5e41b1b8a3f702a9029bd83f
Nathan Chere <git@nathanchere.com.au>
2014-03-27 16:35:06 +1100

    Basic playback working

--- NAudio.Basic/Program.cs
+++ NAudio.Basic/Program.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NAudio.Wave;
 
 namespace NAudio.Basic
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
+            bool quit = false;
+            Console.CancelKeyPress += (sender, eventArgs) => quit = true;
+
+            IWavePlayer waveOutDevice;
+            AudioFileReader audioFileReader;
+
+            waveOutDevice = new WaveOut();
+           
+            audioFileReader = new AudioFileReader("doowackadoo.mp3");
+
+            waveOutDevice.Init(audioFileReader);
+            waveOutDevice.Play();
+
+            Console.WriteLine("Playing doowackadoo; Ctrl+C to quit");
+            while(!quit);
         }
     }
 }
