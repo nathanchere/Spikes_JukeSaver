1faaa7c9de7af91485dc2e30304462c7f8dd47f1
Nathan Chere <git@nathanchere.com.au>
2014-03-27 15:16:06 +1100

    NAudio basic project

---
+++ NAudio.Basic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAudio.Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
