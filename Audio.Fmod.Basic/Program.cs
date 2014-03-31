486d3faab170821b0e53f6a15d3c137746662c2d
Nathan Chere <git@nathanchere.com.au>
2014-03-31 13:28:59 +1100

    Basic file playback; change to .Net 4.0 for FmodSharp

--- Audio.Fmod.Basic/Program.cs
+++ Audio.Fmod.Basic/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Threading;
+using FmodSharp = Linsft.FmodSharp;
 
 namespace Audio.Fmod.Basic
 {
     class Program
-    {        
+    {
         static void Main(string[] args)
         {
+            const string FILE_NAME = @".\doowackadoo.mp3";
+
             var quit = new ManualResetEvent(false);
             Console.CancelKeyPress += (s, a) => {
                 quit.Set();
                 a.Cancel = true;
-            };            
+            };
 
-            Console.WriteLine("FmodSharp test\nPlaying doowackadoo; Ctrl+C to quit");
-            quit.WaitOne();            
+            using (var fmod = new FmodSharp.SoundSystem.SoundSystem())
+            {
+                fmod.Init();
+
+                var audio = fmod.CreateSound(FILE_NAME);
+                fmod.PlaySound(audio);                
+
+                Console.WriteLine("FmodSharp test\nPlaying doowackadoo; Ctrl+C to quit");
+                quit.WaitOne();                
+
+                fmod.CloseSystem();
+            }
         }
     }
 }
