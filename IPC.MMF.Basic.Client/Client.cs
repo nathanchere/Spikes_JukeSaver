2aea0700232c6b0e8f6fd43353489c178f64168e
Nathan Chere <git@nathanchere.com.au>
2014-03-31 12:36:19 +1100

    mmf basic client behaviour

--- IPC.MMF.Basic.Client/Client.cs
+++ IPC.MMF.Basic.Client/Client.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
 
 namespace IPC.MMF
 {
     internal class Client : IDisposable
     {
+        private string lastMessage = null;
+
         public void Dispose()
         {
             
@@ -16,7 +21,31 @@ namespace IPC.MMF
 
         public void Run()
         {
+            while (true)
+            {                
+                Console.WriteLine("Listening to mapped file " + Config.MAPPED_FILE_NAME);
+                using (var map = MemoryMappedFile.CreateNew(Config.MAPPED_FILE_NAME, Config.BufferSize))
+                {                    
+                    bool mutexCreated;
+                    var mutex = new Mutex(true, Config.MAPPED_FILE_NAME, out mutexCreated);
 
+                    using (var stream = map.CreateViewStream())
+                    {
+                        var reader = new BinaryReader(stream);
+                        var message = reader.ReadString();
+
+                        if (!string.IsNullOrEmpty(message) &&
+                            !string.Equals(message, lastMessage))
+                        {
+                            Console.WriteLine(message);
+                            lastMessage = message;
+                        }
+                        
+                    }
+                    mutex.ReleaseMutex();
+                    mutex.WaitOne();                    
+                }
+            }
         }
     }
 }
--- IPC.MMF.Basic.Server/Server.cs
+++ IPC.MMF.Basic.Server/Server.cs
@@ -7,6 +7,7 @@ namespace IPC.MMF
 {
     internal class Server : IDisposable
     {
+        private ulong counter = 0;
         public void Dispose()
         {
 
@@ -18,20 +19,18 @@ namespace IPC.MMF
         }
 
         public void Run()
-        {
-            ulong counter = 0;
+        {            
             while (true)
             {
                 using (var map = MemoryMappedFile.CreateNew(Config.MAPPED_FILE_NAME, Config.BufferSize))
                 {
                     bool mutexCreated;
-                    Mutex mutex = new Mutex(true, Config.MAPPED_FILE_NAME, out mutexCreated);
+                    var mutex = new Mutex(true, Config.MAPPED_FILE_NAME, out mutexCreated);
 
                     using (var stream = map.CreateViewStream())
                     {
                         var writer = new BinaryWriter(stream);
-                        var message = String.Format("{0}: Ping! The time is: {1}",
-                            ++counter, DateTime.Now.ToLongTimeString());
+                        var message = GetMessage();
                         writer.Write(message);
                         Console.WriteLine(message);
                     }
@@ -39,6 +38,12 @@ namespace IPC.MMF
                     mutex.WaitOne();                    
                 }
             }
+        }
+
+        private string GetMessage()
+        {
+            return String.Format("{0}: Ping! The time is: {1}",
+                ++counter, DateTime.Now.ToLongTimeString());
         }
     }
 }
