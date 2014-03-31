9496059de9903fd96ff0559b4849ab77f5854f5e
Nathan Chere <git@nathanchere.com.au>
2014-03-31 13:20:47 +1100

    Fmod basic project

---
+++ Audio.Fmod.Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Audio.Fmod.Basic
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            var quit = new ManualResetEvent(false);
+            Console.CancelKeyPress += (s, a) => {
+                quit.Set();
+                a.Cancel = true;
+            };            
+
+            Console.WriteLine("FmodSharp test\nPlaying doowackadoo; Ctrl+C to quit");
+            quit.WaitOne();            
+        }
+    }
+}
