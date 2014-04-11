22e65c425d561dfff50b166b5470e15483626ac1
Nathan Chere <git@nathanchere.com.au>
2014-04-11 13:11:30 +1000

    MMF serialisation projects

---
+++ Basic/IPC.MMF.Serialization.Client/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+namespace IPC.MMF
+{
+    internal class Client : IDisposable
+    {
+        private string lastMessage = null;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Client()
+        {            
+              
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Listening to mapped file " + Config.MAPPED_FILE_NAME);
+            var mutexName = "mmfclientmutex" + (Guid.NewGuid());
+            Console.Title = mutexName;
+            while (true)
+            {                
+                try
+                {                    
+                    using (var map = MemoryMappedFile.CreateOrOpen(Config.MAPPED_FILE_NAME, Config.BufferSize))
+                    {
+                        bool mutexCreated;
+                        var mutex = new Mutex(true, mutexName, out mutexCreated);
+
+                        using (var stream = map.CreateViewStream())
+                        {
+                            var reader = new BinaryReader(stream);
+                            var message = reader.ReadString();
+
+                            if (!string.IsNullOrEmpty(message) &&
+                                !string.Equals(message, lastMessage))
+                            {
+                                Console.WriteLine(message);
+                                lastMessage = message;
+                            }
+
+                        }
+                        mutex.ReleaseMutex();
+                        mutex.WaitOne();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Basic/IPC.MMF.Serialization.Server/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+namespace IPC.MMF
+{
+    internal class Server : IDisposable
+    {
+        private ulong counter = 0;
+        public void Dispose()
+        {
+
+        }
+
+        public Server()
+        {
+
+        }
+
+        public void Run()
+        {            
+            while (true)
+            {
+                try
+                {
+                    using (var map = MemoryMappedFile.CreateOrOpen(Config.MAPPED_FILE_NAME, Config.BufferSize))
+                    {
+                        bool mutexCreated;
+                        var mutex = new Mutex(true, "mmfservermutex", out mutexCreated);
+
+                        using (var stream = map.CreateViewStream())
+                        {
+                            var writer = new BinaryWriter(stream);
+                            var message = GetMessage();
+                            writer.Write(message);
+                            Console.WriteLine(message);
+                        }
+                        mutex.ReleaseMutex();
+                        mutex.WaitOne();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private string GetMessage()
+        {
+            return String.Format("{0}: Ping! The time is: {1}",
+                ++counter, DateTime.Now.ToLongTimeString());
+        }
+    }
+}
