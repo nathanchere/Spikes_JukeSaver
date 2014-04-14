546c09673ba4f31d0e406a4c274e5bdaaa6604e9
Nathan Chere <git@nathanchere.com.au>
2014-04-14 17:58:40 +1000

    Fix outdated fmod DLLs (nFMOD 0.5 expects 4.44.32 minimum)

---
+++ Basic/Audio.Fmod.SoundGeneration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using nFMOD;
+
+namespace Audio.Fmod.Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quit = new ManualResetEvent(false);
+            Console.CancelKeyPress += (s, a) => {
+                quit.Set();
+                a.Cancel = true;
+            };
+
+            using (var fmod = new FmodSystem())
+            {
+                fmod.Init();                			    
+                using (var oscillator = fmod.CreateDspByType(DspType.Oscillator))
+                {
+                    var channel = fmod.PlayDsp(oscillator);
+
+                    Console.WriteLine("nFMOD test\nGenerating sine wave; Ctrl+C to quit");
+                    quit.WaitOne();
+                }
+                fmod.CloseSystem();
+            }
+        }
+    }
+}
